49745cf1ff47f4af92418a94c130511849112113
Rabah78 <121290096+Rabah78@users.noreply.github.com>
2024-10-20 20:32:03 +0200

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,1 +1,9 @@
+// src/components/ui/Input.jsx
+import React from 'react';
+
+const Input = ({ className = "", ...props }) => (
+  <input className={`input p-2 border rounded ${className}`} {...props} />
+);
+
+export default Input;
 
