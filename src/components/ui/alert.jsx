9a6b4ae38f2d91bd7c5e6855841e2d0fb2c81ea5
Rabah78 <121290096+Rabah78@users.noreply.github.com>
2024-10-20 20:32:44 +0200

    Update alert.jsx

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,1 +1,17 @@
+// src/components/ui/Alert.jsx
+import React from 'react';
+
+export const Alert = ({ children, className = "", ...props }) => (
+  <div className={`alert p-4 border rounded bg-yellow-100 ${className}`} {...props}>
+    {children}
+  </div>
+);
+
+export const AlertDescription = ({ children, className = "", ...props }) => (
+  <p className={`alert-description ${className}`} {...props}>
+    {children}
+  </p>
+);
+
+export default Alert;
 
