8e7e528a52a07326645877fec730c7ac2abfd46f
Rabah78 <121290096+Rabah78@users.noreply.github.com>
2024-10-20 20:32:32 +0200

    Update checkbox.jsx

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -1,1 +1,9 @@
+// src/components/ui/Checkbox.jsx
+import React from 'react';
+
+const Checkbox = ({ className = "", ...props }) => (
+  <input type="checkbox" className={`checkbox ${className}`} {...props} />
+);
+
+export default Checkbox;
 
